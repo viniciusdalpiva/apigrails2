c55de28c2c69c06d4bc0ff6f1900ab7b688543d5
viniciusdalpiva <viniciusdalpivadasilva@gmail.com>
2023-08-12 16:10:27 -0300

    Ajustes de exceções.

--- grails-app/services/projetoapi/DepartamentoService.groovy
+++ grails-app/services/projetoapi/DepartamentoService.groovy
@@ -1,3 +1,37 @@
+//package projetoapi
+//
+//import org.springframework.validation.BindingResult
+//
+//class DepartamentoService {
+//
+//    def listarDepartamentos() {
+//        Departamento.list()
+//    }
+//
+//    def buscarDepartamentoPorId(Long id) {
+//        Departamento.get(id)
+//    }
+//
+//    def criarDepartamento(Map departamentoData) {
+//        def departamento = new Departamento(departamentoData)
+//        departamento.save()
+//        departamento
+//    }
+//
+//    def atualizarDepartamento(Long id, Map departamentoData) {
+//        def departamento = Departamento.get(id)
+//        departamento.properties = departamentoData// as BindingResult
+//        departamento.save()
+//        departamento
+//    }
+//
+//    def excluirDepartamento(Long id) {
+//        def departamento = Departamento.get(id)
+//        departamento.delete()
+//        departamento
+//    }
+//}
+
 package projetoapi
 
 import org.springframework.validation.BindingResult
@@ -9,24 +43,47 @@ class DepartamentoService {
     }
 
     def buscarDepartamentoPorId(Long id) {
-        Departamento.get(id)
+        def departamento = Departamento.get(id)
+        if (!departamento) {
+            throw new IllegalArgumentException("Departamento com ID ${id} não encontrado.")
+        }
+        departamento
     }
 
     def criarDepartamento(Map departamentoData) {
         def departamento = new Departamento(departamentoData)
+        departamento.validate()
+
+        if (departamento.hasErrors()) {
+            throw new IllegalArgumentException(departamento.errors.toString())
+        }
+
         departamento.save()
         departamento
     }
 
     def atualizarDepartamento(Long id, Map departamentoData) {
         def departamento = Departamento.get(id)
-        departamento.properties = departamentoData// as BindingResult
+        if (!departamento) {
+            throw new IllegalArgumentException("Departamento com ID ${id} não encontrado.")
+        }
+
+        departamento.properties = departamentoData
+        departamento.validate()
+
+        if (departamento.hasErrors()) {
+            throw new IllegalArgumentException(departamento.errors.toString())
+        }
+
         departamento.save()
         departamento
     }
 
     def excluirDepartamento(Long id) {
         def departamento = Departamento.get(id)
+        if (!departamento) {
+            throw new IllegalArgumentException("Departamento com ID ${id} não encontrado.")
+        }
         departamento.delete()
         departamento
     }
--- grails-app/services/projetoapi/EmpregadoService.groovy
+++ grails-app/services/projetoapi/EmpregadoService.groovy
@@ -1,6 +1,66 @@
+//package projetoapi
+//
+//class EmpregadoService {
+//
+//    def listarEmpregadosFormatted() {
+//        Empregado.list().collect { empregado ->
+//            formatEmpregado(empregado)
+//        }
+//    }
+//
+//    def buscarEmpregadoFormatadoPorId(Long id) {
+//        def empregado = Empregado.get(id)
+//        formatEmpregado(empregado)
+//    }
+//
+//    def criarEmpregado(Map empregadoData) {
+//        validarDadosObrigatorios(empregadoData)
+//        def empregado = new Empregado(empregadoData)
+//
+//        empregado.save()
+//        formatEmpregado(empregado)
+//    }
+//
+//    def atualizarEmpregado(Long id, Map empregadoData) {
+//        validarDadosObrigatorios(empregadoData)
+//        def empregado = Empregado.get(id)
+//        empregado.properties = empregadoData
+//        empregado.save()
+//        formatEmpregado(empregado)
+//    }
+//
+//    def excluirEmpregado(Long id) {
+//        def empregado = Empregado.get(id)
+//        empregado.delete()
+//        formatEmpregado(empregado)
+//    }
+//
+//    private formatEmpregado(empregado) {
+//        [
+//                id: empregado.id,
+//                matricula: empregado.matricula,
+//                nome: empregado.nome,
+//                dataNascimento: empregado.dataNascimento,
+//                departamento: empregado.departamento.id
+//        ]
+//    }
+//
+//    private validarDadosObrigatorios(Map empregadoData) {
+//        def requiredFields = ['nome', 'matricula', 'dataNascimento', 'departamento']
+//        def missingFields = requiredFields.findAll { !empregadoData.containsKey(it) }
+//
+//        if (missingFields) {
+//            def missingFieldsStr = missingFields.join(', ')
+//            throw new IllegalArgumentException("Não foi possível incluir o empregado, campo(s) '${missingFieldsStr}' não foi(foram) preenchido(s).")
+//        }
+//    }
+//}
+
 package projetoapi
 
 class EmpregadoService {
+
+
 
     def listarEmpregadosFormatted() {
         Empregado.list().collect { empregado ->
@@ -10,12 +70,28 @@ class EmpregadoService {
 
     def buscarEmpregadoFormatadoPorId(Long id) {
         def empregado = Empregado.get(id)
+        if (!empregado) {
+            throw new IllegalArgumentException("Empregado com ID ${id} não encontrado.")
+        }
         formatEmpregado(empregado)
     }
 
     def criarEmpregado(Map empregadoData) {
         validarDadosObrigatorios(empregadoData)
+        def departamentoId = empregadoData.departamento
+        def departamento = Departamento.get(departamentoId)
+
+        if (!departamento) {
+            throw new IllegalArgumentException("Departamento com ID ${departamentoId} não encontrado.")
+        }
+
         def empregado = new Empregado(empregadoData)
+        empregado.departamento = departamento
+        empregado.validate()
+
+        if (empregado.hasErrors()) {
+            throw new IllegalArgumentException(empregado.errors.toString())
+        }
 
         empregado.save()
         formatEmpregado(empregado)
@@ -24,13 +100,34 @@ class EmpregadoService {
     def atualizarEmpregado(Long id, Map empregadoData) {
         validarDadosObrigatorios(empregadoData)
         def empregado = Empregado.get(id)
+        if (!empregado) {
+            throw new IllegalArgumentException("Empregado com ID ${id} não encontrado.")
+        }
+
+        def departamentoId = empregadoData.departamento
+        def departamento = Departamento.get(departamentoId)
+
+        if (!departamento) {
+            throw new IllegalArgumentException("Departamento com ID ${departamentoId} não encontrado.")
+        }
+
         empregado.properties = empregadoData
+        empregado.departamento = departamento
+        empregado.validate()
+
+        if (empregado.hasErrors()) {
+            throw new IllegalArgumentException(empregado.errors.toString())
+        }
+
         empregado.save()
         formatEmpregado(empregado)
     }
 
     def excluirEmpregado(Long id) {
         def empregado = Empregado.get(id)
+        if (!empregado) {
+            throw new IllegalArgumentException("Empregado com ID ${id} não encontrado.")
+        }
         empregado.delete()
         formatEmpregado(empregado)
     }
